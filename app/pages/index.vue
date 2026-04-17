6a55645732f2e7b214c07e2cd986ff336b3b0495
Ismail <151830328+iboukhss@users.noreply.github.com>
2026-04-17 13:26:09 +0200

    ui: Write basic site layout and auth form

---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
+
+const links = ref<ButtonProps[]>([
+  {
+    label: 'Hire Top Talents',
+    to: '/profiles'
+  },
+  {
+    label: 'Apply as a Freelancer',
+    to: '/jobs',
+    variant: 'subtle'
+  }
+])
+</script>
+
+<template>
+  <UPageHero
+    title="Connect with Talents Across the Greater Region"
+    description="Get access to the biggest talent pool from the Greater Region in Luxembourg, France, Belgium and Germany."
+    orientation="horizontal"
+    :links="links"
+  >
+    <img
+      src="/freelancer-office-stock.jpg"
+      class="rounded-xl shadow-xl"
+    >
+  </UPageHero>
+</template>
---
+++ app/pages/register.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import type { AuthFormField } from '@nuxt/ui'
+
+const fields = ref<AuthFormField[]>([
+  {
+    name: 'accountType',
+    type: 'select',
+    label: 'I am a...',
+    defaultValue: 'Freelancer',
+    items: [
+      {
+        label: 'Freelancer',
+        value: 'freelancer'
+      },
+      {
+        label: 'Company',
+        value: 'company'
+      }
+    ]
+  },
+  {
+    name: 'firstName',
+    type: 'text',
+    label: 'First name'
+  },
+  {
+    name: 'lastName',
+    type: 'text',
+    label: 'Last name'
+  },
+  {
+    name: 'email',
+    type: 'text',
+    label: 'Email'
+  },
+  {
+    name: 'password',
+    type: 'password',
+    label: 'Password'
+  },
+  {
+    name: 'country',
+    type: 'select',
+    label: 'Country',
+    placeholder: 'Select country',
+    items: [
+      {
+        label: 'Belgium',
+        value: 'be'
+      },
+      {
+        label: 'France',
+        value: 'fr'
+      },
+      {
+        label: 'Germany',
+        value: 'de'
+      },
+      {
+        label: 'Luxembourg',
+        value: 'lu'
+      }
+    ]
+  }
+])
+</script>
+
+<template>
+  <div class="flex flex-col items-center p-4">
+    <UAuthForm
+      title="Register"
+      description="Create a new LuxLink account."
+      :fields="fields"
+      class="max-w-md"
+    />
+  </div>
+</template>
